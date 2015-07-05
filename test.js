a33c43eea0148cb4348cd43f90eb895e87103974
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-05 23:36:15 +0530

    Add more tests, change test name.
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- test.js
+++ test.js
@@ -7,6 +7,8 @@ it('should sort an array', function () {
   assert.deepEqual(mathSort([100, 10, 20, 30, 1, 5]), [1, 5, 10, 20, 30, 100]);
 });
 
-it('should sort a set of numbers', function () {
+it('should return undefined on anything other than arrays', function () {
   assert.deepEqual(mathSort(100, 10, 20, 30, 1, 5), undefined);
+  assert.deepEqual(mathSort('some word here'), undefined);
+  assert.deepEqual(mathSort(100), undefined);
 });
